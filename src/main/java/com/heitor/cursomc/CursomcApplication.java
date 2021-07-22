85011b5fbdb003d030767769a665e4f7b54b803f
Heitor <heitorhfbr@gmail.com>
2021-07-22 13:54:54 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/heitor/cursomc/CursomcApplication.java
+++ src/main/java/com/heitor/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.heitor.cursomc;
 
 import com.heitor.cursomc.domain.Categoria;
+import com.heitor.cursomc.domain.Produto;
 import com.heitor.cursomc.reposotories.CategoriaRepository;
+import com.heitor.cursomc.reposotories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -26,6 +31,17 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat2 = new Categoria(null, "Escritório");
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		p1.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 
 	}
 }
