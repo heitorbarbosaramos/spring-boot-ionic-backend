f812580d13f37c93421b2c18595cad586022f33c
Heitor <heitorhfbr@gmail.com>
2021-07-22 15:41:20 -0300

    Cidade e Estado

--- src/main/java/com/heitor/cursomc/CursomcApplication.java
+++ src/main/java/com/heitor/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.heitor.cursomc;
 
 import com.heitor.cursomc.domain.Categoria;
+import com.heitor.cursomc.domain.Cidade;
+import com.heitor.cursomc.domain.Estado;
 import com.heitor.cursomc.domain.Produto;
 import com.heitor.cursomc.reposotories.CategoriaRepository;
+import com.heitor.cursomc.reposotories.CidadeRepository;
+import com.heitor.cursomc.reposotories.EstadoRepository;
 import com.heitor.cursomc.reposotories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -41,6 +51,19 @@ public class CursomcApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat2));
 
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulos");
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+
 
 
 	}
--- src/main/java/com/heitor/cursomc/domain/Estado.java
+++ src/main/java/com/heitor/cursomc/domain/Estado.java
@@ -1,8 +1,5 @@
 package com.heitor.cursomc.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -10,21 +7,17 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-public class Categoria implements Serializable {
+public class Estado implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
-    @JsonManagedReference
-    @ManyToMany(mappedBy = "categorias")
-    private List<Produto> produtos = new ArrayList<>();
+    @OneToMany(mappedBy = "estado")
+    private List<Cidade> cidades = new ArrayList<>();
 
-    public Categoria(){
-    }
-
-    public Categoria(Integer id, String nome) {
+    public Estado(Integer id, String nome) {
         this.id = id;
         this.nome = nome;
     }
@@ -45,20 +38,20 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
+    public List<Cidade> getCidades() {
+        return cidades;
     }
 
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Categoria categoria = (Categoria) o;
-        return Objects.equals(id, categoria.id);
+        Estado estado = (Estado) o;
+        return id.equals(estado.id);
     }
 
     @Override
