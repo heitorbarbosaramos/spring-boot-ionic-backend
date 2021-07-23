b47e00108ff9afaa9cdd246ccfe40f88577af5f9
Heitor <heitorhfbr@gmail.com>
2021-07-23 09:42:25 -0300

    Resolvendo imports da ResourcesExcepriosHandler

--- src/main/java/com/heitor/cursomc/exceptions/ResourcesExceptionsHandler.java
+++ src/main/java/com/heitor/cursomc/exceptions/ResourcesExceptionsHandler.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @ControllerAdvice
 public class ResourcesExceptionsHandler {
