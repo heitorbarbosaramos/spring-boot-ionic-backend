76b44d5d7fcd887fdb2b7bf5082896413f6b0e4a
Heitor <heitorhfbr@gmail.com>
2021-07-25 00:42:28 -0300

    Validacao customizada para insercao de email nao repetidos

--- src/main/java/com/heitor/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/heitor/cursomc/services/validation/ClienteInsertValidator.java
@@ -3,7 +3,9 @@ package com.heitor.cursomc.services.validation;
 import com.heitor.cursomc.domain.dto.ClienteNewDTO;
 import com.heitor.cursomc.enums.TipoCliente;
 import com.heitor.cursomc.exceptions.FieldMessage;
+import com.heitor.cursomc.reposotories.ClienteRepository;
 import com.heitor.cursomc.services.utils.ValidaDocumentos;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +13,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
@@ -27,6 +33,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !ValidaDocumentos.validaCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        if(repo.findByEmail(objDto.getEmail()) != null){
+            list.add(new FieldMessage("email", "email já cadastro"));
         }
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
